dcf06fcee88181c0eb11217d345bc0475a68d9d0
supermanklk <739367755@qq.com>
2023-02-16 10:53:49 +0800

    智能合约-存储

---
+++ contracts/DataStorage5.sol
@@ -0,0 +1,27 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.4;
+
+contract DataStorage {
+    uint[] x = [1, 2, 3];
+    
+    function fStorage() public{
+        uint[] storage xStorage = x;
+        xStorage[0] = 100;
+    }
+
+    function fMemory() public view {
+        uint[] memory xMemory = x;
+        xMemory[0] = 100;
+        xMemory[1] 200;
+        uint[] memory xMemory2 = x;
+        xMemory2[0] = 300;
+    }
+
+    function fCalldata(uint[] calldata _x) public pure returns(uint[] calldata) {
+        return (_x);
+    }
+
+    
+
+    function 
+}
